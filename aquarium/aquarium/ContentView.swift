27a245cc7ae28f405b00eb1e857bf0395511ddfc
raquelsilvax <raquelsilvadosanto@hotmail.com>
2024-05-13 17:24:53 -0300

    Cria arquivos iniciais do projeto

---
+++ aquarium/aquarium/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aquarium
+//
+//  Created by Raquel Silva dos Santos on 13/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aquarium/aquarium/aquariumApp.swift
@@ -0,0 +1,17 @@
+//
+//  aquariumApp.swift
+//  aquarium
+//
+//  Created by Raquel Silva dos Santos on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct aquariumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
