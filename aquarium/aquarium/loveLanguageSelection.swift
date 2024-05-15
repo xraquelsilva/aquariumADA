00ba99ffaf885d1380068597ccb6b84e06b887fc
raquelsilvax <raquelsilvadosanto@hotmail.com>
2024-05-15 14:05:11 -0300

    Adiciona slider e cria base para pull-down button (cases em novo arquivo)

---
+++ aquarium/aquarium/loveLanguageSelection.swift
@@ -0,0 +1,15 @@
+//
+//  loveLanguageSelection.swift
+//  aquarium
+//
+//  Created by Raquel Silva dos Santos on 15/05/24.
+//
+
+import Foundation
+
+enum loveLanguage: String, CaseIterable {
+    case top
+    case bottom
+    
+    
+}
